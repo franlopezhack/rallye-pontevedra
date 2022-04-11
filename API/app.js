c3859c309dd741ef798efeadc7d1e52077922e14
Fran <fransanxenxo@gmail.com>
2022-04-11 17:14:09 +0100

    feat: error handling

--- API/app.js
+++ API/app.js
@@ -1,11 +1,17 @@
 require('dotenv').config()
 
 const express = require('express')
-
-const app = express();
+const logger = require('morgan');
 
 /*Congis*/
 require('./config/db.config');
+
+const app = express();
+
+app.use(logger('dev'));
+
+const routes = require('./config/routes.config')
+app.use('/api', routes)
 
 
 const port = process.env.PORT || 3001
