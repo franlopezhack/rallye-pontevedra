5b501d5a7dbb502917f159526d2a85c1d4e340fb
Fran <fransanxenxo@gmail.com>
2022-04-25 09:44:18 +0100

    new web

--- WEB/src/components/HomeCarrousel/HomeCarousel.jsx
+++ WEB/src/components/HomeCarrousel/HomeCarousel.jsx
@@ -1,7 +1,7 @@
 import './HomeCarousel.css'
-import img1 from '../../assets/images/CarouselImages/img1.jpeg'
-// import img2 from '../../assets/images/CarouselImages/img2.JPG'
-// import img3 from '../../assets/images/CarouselImages/img3.JPG'
+import img1 from '../../assets/images/CarouselImages/img1.JPG'
+import img2 from '../../assets/images/CarouselImages/img2.JPG'
+import img3 from '../../assets/images/CarouselImages/img3.JPG'
 
 function HomeCarousel() {
     return (
@@ -12,10 +12,10 @@ function HomeCarousel() {
                         <img src={img1} className="d-block w-100" alt={img1}/>
                     </div>
                     <div className="carousel-item">
-                        <img src={img1} className="d-block w-100" alt={img1}/>
+                        <img src={img2} className="d-block w-100" alt={img2}/>
                     </div>
                     <div className="carousel-item">
-                        <img src={img1} className="d-block w-100" alt={img1}/>
+                        <img src={img3} className="d-block w-100" alt={img3}/>
                     </div>
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
