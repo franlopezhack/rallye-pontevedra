b27ffdd33942ea6f78f5835a28cc5ee757ea447a
Fran <fransanxenxo@gmail.com>
2022-04-30 21:41:04 +0100

    feat: text animation carousel component

--- WEB/src/components/HomeCarrousel/HomeCarousel.jsx
+++ WEB/src/components/HomeCarrousel/HomeCarousel.jsx
@@ -3,19 +3,23 @@ import img1 from '../../assets/images/CarouselImages/img1.JPG'
 import img2 from '../../assets/images/CarouselImages/img2.JPG'
 import img3 from '../../assets/images/CarouselImages/img3.JPG'
 
+
 function HomeCarousel() {
     return (
         <div className="home-carousel">
             <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
                 <div className="carousel-inner">
                     <div className="carousel-item active">
-                        <img src={img1} className="d-block w-100" alt={img1}/>
+                        <img src={img1} className="d-block w-100" alt={img1} />
+                        <div className='text-animation'>
+                            <h2>IV Rallye de Pontevedra 2022</h2>
+                        </div>
                     </div>
                     <div className="carousel-item">
-                        <img src={img2} className="d-block w-100" alt={img2}/>
+                        <img src={img2} className="d-block w-100" alt={img2} />
                     </div>
                     <div className="carousel-item">
-                        <img src={img3} className="d-block w-100" alt={img3}/>
+                        <img src={img3} className="d-block w-100" alt={img3} />
                     </div>
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
