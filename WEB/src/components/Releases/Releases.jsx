45dfcd8cb1e8404002d50d979448d59ce9183b23
Fran <fransanxenxo@gmail.com>
2022-04-30 20:10:38 +0100

    feat: css in Release  view

--- WEB/src/components/Releases/Releases.jsx
+++ WEB/src/components/Releases/Releases.jsx
@@ -16,32 +16,37 @@ function Releases() {
             })
     }, []);
 
-  
+
     if (!releases) {
         return null
     };
 
 
     return (
-        <div className="release-card d-flex flex-wrap">
-            {releases.map(release =>
-                <Link to={`/releases/${release.id}`} style={{textDecoration:'none'}} key={release.title}>
-                    <div className="card card-release mb-5 me-5" >
-                        <div className="row g-0">
-                            <div className="col-md-4" id='img-card'>
-                                <img src={release.avatar} className="img-fluid rounded-start" alt={release.image} />
-                            </div>
-                            <div className="col-md-8 text-release">
-                                <div className="card-body">
-                                    <h5 className="card-title">{release.title}</h5>
-                                    <p className="card-text">{release.description.slice(0, 100)}...</p>
+        <>
+            <h1>Noticias</h1>
+            <hr/>
+            <div className="release-card d-flex flex-wrap justify-content-center">
+                {releases.map(release =>
+                    <Link to={`/releases/${release.id}`} style={{ textDecoration: 'none' }} key={release.title}>
+                        <div className="card card-release mb-5 me-5" >
+                            <div className="row g-0">
+                                <div className="col-md-4" id='img-card'>
+                                    <img src={release.avatar} className="img-fluid rounded-start" alt={release.image} />
+                                </div>
+                                <div className="col-md-8 text-release">
+                                    <div className="card-body">
+                                        <h5 className="card-title">{release.title}</h5>
+                                        <p className="card-text">{release.description.slice(0, 100)}...</p>
+                                        {release.createdAt.slice(0,10)}
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </Link>
-            )}
-        </div>
+                    </Link>
+                )}
+            </div>
+        </>
     )
 }
 
