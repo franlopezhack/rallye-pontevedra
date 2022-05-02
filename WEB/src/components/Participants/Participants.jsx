ac18555877b799688a2c3ac387c930bca53e23f6
Fran <fransanxenxo@gmail.com>
2022-05-02 13:42:52 +0100

    new web

--- WEB/src/components/Participants/Participants.jsx
+++ WEB/src/components/Participants/Participants.jsx
@@ -1,9 +1,19 @@
 import './Participants.css'
+import placa from '../../assets/logo/placa.png'
 
 function Participants() {
+
     return (
-        <div className="info-text">
-            <h1>Próximamente mas información</h1>
+        <div className="info-participants mt-5 mb-5">
+            <div className='container-participants text-center'>
+                <h4 className='mb-5'>Ya están abiertas las inscripciones para el IV Rally de Pontevedra, pincha en el botón para acceder al formulario de inscripción</h4>
+                <img className='mt-5 mb-5' src={placa} alt={placa} />
+                <div>
+                    <a href='https://docs.google.com/forms/d/e/1FAIpQLSe4ahs1xp5kY_eIWAyn22PdaUaVLV1qFH6jdhxb6PFOH9v9Zg/viewform' >
+                        <p className='btn btn-primary'>INSCRIPCIONES</p>
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
--- WEB/src/pages/Home/Home.jsx
+++ WEB/src/pages/Home/Home.jsx
@@ -112,6 +112,11 @@ function Home() {
             <div>
                 <BulletinBoard />
             </div>
+            <div className="marquee-warning  mt-3">
+                <Marquee gradientWidth={4}>
+                    <h3 className=""> INSCRIPCIONES ABIERTAS: <small>puedes realizar la inscripción accediendo a la seccion PARTICIPANTES</small></h3>
+                </Marquee>
+            </div>
             <div>
                 <nav className="nav d-flex justify-content-center">
                     <p className="d-flex">Ultimas Noticias</p>
