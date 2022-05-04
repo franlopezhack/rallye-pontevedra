fe49954b6eeb4335d5035e3cc6f961d048cdcdd8
Fran <fransanxenxo@gmail.com>
2022-05-04 21:57:04 +0100

    feat:add animation in img2 carousel

--- WEB/src/components/HomeCarrousel/HomeCarousel.jsx
+++ WEB/src/components/HomeCarrousel/HomeCarousel.jsx
@@ -1,8 +1,9 @@
 import './HomeCarousel.css'
 import img1 from '../../assets/images/CarouselImages/img1.JPG'
 import img2 from '../../assets/images/CarouselImages/img2.JPG'
-import img3 from '../../assets/images/CarouselImages/img3.JPG'
+import img3 from '../../assets/images/CarouselImages/foto ruta.jpeg'
 import placa from '../../assets/logo/placa.png'
+import sanxenxo from '../../assets/images/Corporaciones/MSANXENXO.L.COLOR_1-removebg-preview.png'
 
 
 function HomeCarousel() {
@@ -20,10 +21,18 @@ function HomeCarousel() {
                         </div>
                     </div>
                     <div className="carousel-item">
-                        <img src={img2} className="d-block w-100" alt={img2} />
+                        <img src={img3} className="d-block w-100" alt={img2} />
+                        <div className='text-img2'>
+                        <img className='animate__animated animate__backInLeft animate__delay-2s animate__slower' src={sanxenxo} alt={sanxenxo} />
+                            <h2 className='animate__animated animate__bounceInDown animate__delay-2s animate__slower'>
+                            <div>#Sanxenxo,</div> 
+                            <div>algo mas</div> 
+                            <div>que sol y playa</div>
+                            </h2>
+                        </div>
                     </div>
                     <div className="carousel-item">
-                        <img src={img3} className="d-block w-100" alt={img3} />
+                        <img src={img2} className="d-block w-100" alt={img3} />
                     </div>
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
