eeab98e3003f2f23ec3a7fb88192eff37a04eab4
Fran <fransanxenxo@gmail.com>
2022-05-09 18:57:03 +0100

    feat: responsive sponsors and carousel

--- WEB/src/components/MarqueSponsors/MarqueSponsors.jsx
+++ WEB/src/components/MarqueSponsors/MarqueSponsors.jsx
@@ -1,4 +1,3 @@
-import Marquee from 'react-fast-marquee'
 import './MarqueSponsors.css'
 import Ausavil from '../../assets/images/MarqueeSponsors/Ausavil.jpg'
 import Bamarti from '../../assets/images/MarqueeSponsors/Bamarti.jpg'
@@ -12,43 +11,42 @@ import RsAuto from '../../assets/images/MarqueeSponsors/RS Premium Car.jpg'
 
 function MarqueeSponsors() {
     return (
-            <Marquee speed={0} gradient={false}>
-                <section className='sponsors-section'>
-                    <ul className='d-flex flex-wrap justify-content-between'>
-                        <a href='https://www.sanxenxo.es/index.php/es/' className='image-wrapper'>
+   
+                <div className='sponsors-list'>
+              
+                        <a href='https://www.sanxenxo.es/index.php/es/' className='image-sponsor'>
                             <img src={Bamarti} alt={Bamarti} />
                         </a>
 
-                        <a href='http://www.concellodemeano.com/ga/inicio' className='image-wrapper'>
+                        <a href='http://www.concellodemeano.com/ga/inicio' className='image-sponsor'>
                             <img src={Ausavil} alt={Ausavil} />
                         </a>
 
-                        <a href='http://www.cerdedo-cotobade.gal/es/' className='image-wrapper'>
+                        <a href='http://www.cerdedo-cotobade.gal/es/' className='image-sponsor'>
                             <img src={Bonpollo} alt={Bonpollo} />
                         </a>
 
-                        <a href='https://campolameiro.org/es/' className='image-wrapper'>
+                        <a href='https://campolameiro.org/es/' className='image-sponsor'>
                             <img src={Caetano} alt={Caetano} />
                         </a>
-
-                        <a href='https://morana.org/' className='image-wrapper'>
+                   
+                        <a href='https://morana.org/' className='image-sponsor'>
                             <img src={Furgoline} alt={Furgoline} />
                         </a>
 
-                        <a href='https://deporte.xunta.gal/' className='image-wrapper'>
+                        <a href='https://deporte.xunta.gal/' className='image-sponsor'>
                             <img src={Nautico} alt={Nautico} />
                         </a>
 
-                        <a href='https://www.depo.gal/es/' className='image-wrapper'>
+                        <a href='https://www.depo.gal/es/' className='image-sponsor'>
                             <img src={Dakar} alt={Dakar} />
                         </a>
 
-                        <a href='https://www.xunta.gal/portada' className='image-wrapper'>
+                        <a href='https://www.xunta.gal/portada' className='image-sponsor'>
                             <img src={RsAuto} alt={RsAuto} />
                         </a>
-                    </ul>
-                </section>
-            </Marquee>
+                    </div>
+              
     )
 }
 
