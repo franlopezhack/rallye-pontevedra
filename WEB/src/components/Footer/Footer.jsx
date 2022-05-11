f3e8c0368d1438040e383d74aa03246ca9793ecd
Fran <fransanxenxo@gmail.com>
2022-05-11 18:27:55 +0100

    feat: add logo

--- WEB/src/components/Footer/Footer.jsx
+++ WEB/src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import './Footer.css'
 import escudería from '../../assets/logo/logo-escudería.png'
-import bruma from '../../assets/images/Logotipo Azul Oscuro Tecnología y Gaming.png'
+// import bruma from '../../assets/images/Logotipo Azul Oscuro Tecnología y Gaming.png'
 
 
 function Footer() {
@@ -33,9 +33,9 @@ function Footer() {
                         </ul>
                     </div>
                 </div>
-                <div className='d-flex justify-content-center'>
+                {/* <div className='d-flex justify-content-center'>
                     <img className='bruma-img' src={bruma} alt={bruma} />
-                </div>
+                </div> */}
             </footer>
         </>
 
