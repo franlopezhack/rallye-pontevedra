c43b0e2a8d9b4cf459de344fa99c84d214f3da61
Fran <fransanxenxo@gmail.com>
2022-06-14 19:45:01 +0100

    feat: replace routes

--- API/controllers/communicating.controller.js
+++ API/controllers/communicating.controller.js
@@ -1,22 +1,22 @@
-const Communicate = require('../models/communicating.model')
+const Communicates = require('../models/communicating.model')
 const createError = require('http-errors')
 
 
 module.exports.list = (req, res, next) => {
-    Communicate.find()
+    Communicates.find()
         .then((communicate) => res.status(201).json(communicate))
         .catch((error) => next(error))
 }
 
 module.exports.create = (req, res, next) => {
-    Communicate.create(req.body)
+    Communicates.create(req.body)
         .then(communicate => res.json(communicate))
         .catch(error => next(error))
 }
 
 
 module.exports.detail = (req, res, next) => {
-    Communicate.findById(req.params.id)
+    Communicates.findById(req.params.id)
         .then(communicate => {
             if (!communicate) {
                 next(createError(404, 'not found'))
@@ -28,7 +28,7 @@ module.exports.detail = (req, res, next) => {
 }
 
 module.exports.delete = (req, res, next) => {
-    Communicate.findByIdAndDelete(req.params.id)
+    Communicates.findByIdAndDelete(req.params.id)
         .then(communicate => {
             if (!communicate) {
                 next(createError(404, `Map ${req.params.id} not found`))
